00bed94dfdd7fa41e7a89287f3bea21bf89c78cd
NAGESWAR NAIDU T <37297869+NaaaaaG@users.noreply.github.com>
2019-04-19 19:49:12 +0530

    Update nag.c

--- nag.c
+++ nag.c
@@ -1,22 +1,22 @@
 #include <stdio.h> 
 int main() 
 { 
-    int n, m, i, j, k; 
+    int n, m, i, j, k,a[5][3],max[5][3],avail[3]; 
     n = 5; // Number of processes 
     m = 3; // Number of resources 
-    int a[5][3] = {     { 0, 1, 0 }, // P0    // Allocation Matrix 
+    a[5][3] = {         { 0, 1, 0 }, // P0    // Allocation Matrix 
                         { 2, 0, 0 }, // P1 
                         { 3, 0, 2 }, // P2 
                         { 2, 1, 1 }, // P3 
                         { 0, 0, 2 } }; // P4 
   
-    int max[5][3] = { { 7, 5, 3 }, // P0    // MAX Matrix 
+    max[5][3] = {     { 7, 5, 3 }, // P0    // MAX Matrix 
                       { 3, 2, 2 }, // P1 
                       { 9, 0, 2 }, // P2 
                       { 2, 2, 2 }, // P3 
                       { 4, 3, 3 } }; // P4 
   
-    int avail[3] = { 3, 3, 2 }; // Available Resources 
+    avail[3] = { 3, 3, 2 }; // Available Resources 
   
     int f[n], ans[n], ind = 0; 
     for (k = 0; k < n; k++) { 
